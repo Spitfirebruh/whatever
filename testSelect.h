d208e2fdbe15d61f2bf5f94ad35da83e8c3270c2
Deaven Garcia <166474547+Spitfirebruh@users.noreply.github.com>
2024-11-28 18:02:57 -0800

    Update testSelect.h

--- testSelect.h
+++ testSelect.h
@@ -1,16 +1,1 @@
-// testSelect.h | A Header File That Handles Selection For Test Purposes.
-#ifndef TESTSELECT_H
-#define TESTSELECT_H
-#include <iostream>
-#include <string>
-#include "enemy.h"
-#include "items.h"
-#include "player.h"
-using namespace std;
-
-class testSelect {
-public:
-	void selectTestOption(Player& player, Enemy& enemy, invItems& invitems);
-	void chooseTurnCombat(Player& player, Enemy& enemy, invItems& invitems, int currentTurn, int battleChoice, int enemyBChoice);
-};
-#endif
+// battle.h is the new version of this file
